1bb39dc18e6b38bba1e18eb8f810e602c17918de
jose <87987486+jlarafl@users.noreply.github.com>
2023-08-16 12:01:12 -0400

    updated header component

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -5,7 +5,11 @@ import './header.css'
 const Header = () => {
     return (
         <div className="header__container">
-            <div className="header__container-image">
+            <div className="header__container-overlay">
+                <div className="header__container-overlay-text">
+                    <h1>Header TExt</h1>
+                    <h3>Describe text</h3>
+                </div>
             </div>
         </div>
     )
