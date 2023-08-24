e2d5bfd2b3f21639ca105df24ac481c906b96cd6
jose <87987486+jlarafl@users.noreply.github.com>
2023-08-24 11:25:45 -0400

    added user, model, controller for login/signup

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 
 const express = require('express');
-const workoutRoutes = require('./routes/workouts');
 const mongoose = require('mongoose');
+const workoutRoutes = require('./routes/workouts');
+const userRoutes = require('./routes/user');
+
 
 const app = express();
 
@@ -13,6 +15,7 @@ app.use((req, res, next) =>{
 });
 
 app.use('/api/workouts', workoutRoutes);
+app.use('/api/user', userRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
